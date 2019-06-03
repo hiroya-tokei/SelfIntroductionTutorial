c5b822224fd72b7a44a94067df318b5784d1baa3
hiroya Akita <hiroya_akita@kyoso.co.jp>
2019-06-03 10:42:56 +0900

    feat: API Gatewayからデータを取るように修正

--- Vue/self-introduction-app/src/components/SelfIntroduction.vue
+++ Vue/self-introduction-app/src/components/SelfIntroduction.vue
@@ -15,11 +15,17 @@
         p.subheading(style="white-space:pre-wrap") {{ selfIntroductionMessage }}
       v-flex(mb-5="" xs12="")
         h2.headline.font-weight-bold.mb-3 趣味は？
-        p.subheading.ma-1(v-for="(hoby, i) in hobyList" :key="i") {{ hoby }}
+        p.subheading.ma-1(v-for="(hobby, i) in hobbyList" :key="i") {{ hobby }}
+      v-flex(mb-5="" xs12="")
+        v-btn(@click="updateUserData()") 更新
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import axios from "axios";
+
+const API_ENDPOINT =
+  "https://XXXXXXXX.execute-api.ap-northeast-1.amazonaws.com/prod/";
 
 @Component({})
 export default class SelfIntroduction extends Vue {
@@ -28,15 +34,57 @@ export default class SelfIntroduction extends Vue {
   selfIntroductionMessage: string = `AWSを使ったサービスをメインに提供しています
     また、フロントエンドも少し触ります
     あと、Linux大好きです。特に、DebianとUbuntuが好きです`;
-  hobyList: string[] = ["寝ることー", "読書ー", "Linuxを触って遊ぶことー"];
+  hobbyList: string[] = ["寝ることー", "読書ー", "Linuxを触って遊ぶことー"];
 
-  public getSkillColorList(index: number) {
+  /**
+   * DOMの構築後に実行される
+   */
+  private mounted() {
+    const self: SelfIntroduction = this;
+    axios
+      .post(API_ENDPOINT + "user-data", { method: "get" })
+      .then((response: any) => {
+        window.console.log(response);
+        self.hobbyList = response["data"]["body"]["hobbyList"];
+        self.selfIntroductionMessage =
+          response["data"]["body"]["selfIntroductionMessage"];
+      })
+      .catch((err: any) => {
+        window.console.error(err);
+      });
+  }
+
+  /**
+   * スキルリストを表示するときの色を取得する
+   * @param {number} index
+   * @return {string} 色
+   */
+  public getSkillColorList(index: number): string {
     if (index > this.skillColorList.length) {
       // 配列の最後の要素を返す
       return this.skillColorList.slice(-1)[0];
     } else {
       return this.skillColorList[index];
     }
+  }
+
+  /**
+   * ユーザー情報を更新する
+   */
+  public updateUserData(): void {
+    let hobbyList: string[] = ["test1", "test2", "test3"];
+    axios
+      .post(API_ENDPOINT + "user-data", {
+        method: "update",
+        hobbyList: hobbyList,
+        selfIntroductionMessage: "今日も一日頑張りましょう！！"
+      })
+      .then((response: any) => {
+        window.console.log(response);
+      })
+      .catch((err: any) => {
+        window.console.error(err);
+      });
   }
 }
 </script>
